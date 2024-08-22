49739ea7f3ffb5fbc09bc9613bb990b8c529a9c1
Khang <khangtl3@uci.edu>
2024-08-21 22:53:26 -0700

    Small Fix: style search bar

--- frontend/src/components/Recipes/searchBar.tsx
+++ frontend/src/components/Recipes/searchBar.tsx
@@ -8,24 +8,33 @@ import SearchIcon from "@mui/icons-material/Search";
 //import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
+import { Box } from '@mui/material';
 
 const SearchBar = () => (
-	<Paper
-	component="form"
-	sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+	<Box
+	sx={{
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	}}
 >
-	<InputBase
-		sx={{ ml: 1, flex: 1 }}
-		placeholder="Search"
-		onInput={(e) => {
-			const target = e.target as HTMLInputElement;
-			console.log(target.value);
-		}}
-	/>
-	<IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-		<SearchIcon />
-	</IconButton>
-</Paper>
+	<Paper
+		component="form"
+		sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '50%' }}
+	>
+		<InputBase
+			sx={{ ml: 1, flex: 1 }}
+			placeholder="Search"
+			onInput={(e) => {
+				const target = e.target as HTMLInputElement;
+				console.log(target.value);
+			}}
+		/>
+		<IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+			<SearchIcon />
+		</IconButton>
+	</Paper>
+</Box>
 	
 );
 
